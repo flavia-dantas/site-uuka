453743f2dbaaf1b89ec1fa30e1713436b5641bb2
flavia-dantas <flavia.dantas2@hotmail.com>
2025-10-10 21:30:35 -0300

    feat: create testimonials section

--- src/sections/Testimonials/testimonials-section.tsx
+++ src/sections/Testimonials/testimonials-section.tsx
@@ -1,7 +1,13 @@
 export function TestimonialsSection() {
   return (
-    <div>
-      <h2>depoimentos</h2>
-    </div>
+    <section id="depoimentos" className="pt-[72px] h-screen w-full bg-white text-black snap-start">
+      <div className="w-full flex flex-col justify-between p-6 md:p-12 lg:p-16">
+
+
+        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-semibold text-[#F59F23]">
+          depoimentos
+        </h2>
+      </div>
+    </section>
   );
-}
+} 
