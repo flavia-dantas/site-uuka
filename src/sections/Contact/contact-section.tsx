7ae23476318f5a822de85562fa3131bcd1c502c0
flavia-dantas <flavia.dantas2@hotmail.com>
2025-10-10 21:30:49 -0300

    feat: create contact section

--- src/sections/Contact/contact-section.tsx
+++ src/sections/Contact/contact-section.tsx
@@ -1,7 +1,12 @@
 export function ContactSection() {
   return (
-    <div>
-      <h2>contato</h2>
-    </div>
+    <section id="contatos" className="pt-[72px] h-screen w-full snap-start bg-[#F59F23] text-white">
+      <div className="h-[calc(100vh-72px)] w-full flex flex-col justify-between p-6 md:p-12 lg:p-16">
+
+        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-semibold text-black">
+          contatos
+        </h2>
+      </div>
+    </section>
   );
 }
