d063476c8a2b9603339d9b1f6ce0a8cad4a8b887
flavia-dantas <flavia.dantas2@hotmail.com>
2025-10-10 21:30:12 -0300

    feat: create partnerships section

---
+++ src/sections/Partnerships/partnerships-section.tsx
@@ -0,0 +1,13 @@
+export function PartnershipsSection() {
+  return (
+    <section id="parcerias" className="pt-[72px] h-screen w-full snap-start bg-[#F59F23] text-white">
+      <div className="w-full flex flex-col justify-between p-6 md:p-12 lg:p-16">
+
+
+        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-semibold text-black">
+          parcerias
+        </h2>
+      </div>
+    </section>
+  );
+}
